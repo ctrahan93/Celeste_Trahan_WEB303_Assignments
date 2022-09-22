edcec373448de492aaf6b85a064d309db3b4f4f2
ctrahan93 <100620806+ctrahan93@users.noreply.github.com>
2022-09-21 20:29:04 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+/*
+	WEB 303 Assignment 1 - jQuery
+	{Celeste Trahan}
+	Student #: 0637756 
+*/
+
+
+
+$('li').on('click', function(e) {var eventType = change;
+});
+
+$('li').on('click', function(e) {var eventType = change;
+});
+
+var $yearly-salary = $('#yearly-salary');
+var $percent = $('#percent');
+var $amount = $('span#amount');
+
+function(e) = (($yearly-salary * $percent) / 100); 
+
+$("span#amount").replaceWith(function(e));
+
+}}; 
+
