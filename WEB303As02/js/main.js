52fb20194fad2705e97611ed214b2442a3b166b5
ctrahan93 <100620806+ctrahan93@users.noreply.github.com>
2022-10-05 23:51:04 -0400

    Add files via upload

---
+++ WEB303As02/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+/* WEB 303 Assignment 2 
+Name: Celeste Trahan
+Student #: 0637756 
+*/ 
+
+$("button").click(function() {
+
+$('#prospect').load('prospect.html', function());
+$('#convert').load('convert.html #convert');
+$('#retain').load('retain.html #retain');
+
+});
+
+
+
+});
+
